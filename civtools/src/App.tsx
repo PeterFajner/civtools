66c872a782c221b8364449e50a2908a23ceb6c4e
Peter Fajner <peter@pfaj.ca>
2024-04-27 16:01:50 -0300

    Add manual discounts and a discount total display

--- civtools/src/App.tsx
+++ civtools/src/App.tsx
@@ -1,9 +1,9 @@
 import { useMemo, useState } from "react";
 import "./App.css";
-import { CivCard, DiscountTuple, calculateDiscount } from "./CivCard";
+import { CivCard, Color, DiscountTuple, calculateDiscount } from "./CivCard";
 import { CardList } from "./CivCardList.json";
+import { Card, CardColor } from "./components/Card";
 import findSets from "./findSets";
-import { Card } from "./components/Card";
 
 const sortAlphabetically = (a: CivCard, b: CivCard) =>
   a.cardName < b.cardName ? -1 : a.cardName > b.cardName ? 1 : 0;
@@ -52,11 +52,46 @@ const ShopSet = ({
   );
 };
 
+const ManualDiscount = ({
+  color,
+  discount,
+  setDiscount,
+  allDiscounts,
+}: {
+  color: string;
+  discount: number;
+  setDiscount: (value: number) => void;
+  allDiscounts: DiscountTuple[];
+}) => {
+  return (
+    <div className="manualDiscount">
+      <CardColor color={color} />
+      <input
+        type="text"
+        value={discount}
+        onChange={(e) => setDiscount(Number(e.target.value))}
+        min={0}
+        max={100}
+      />
+      <span>
+        {allDiscounts
+          .filter((d) => d.color === color)
+          .reduce((total, d) => total + d.discount, 0)}
+      </span>
+    </div>
+  );
+};
+
+const DISCOUNT_NAMES = ["RED", "ORANGE", "YELLOW", "GREEN", "BLUE"];
+
 const App = () => {
   const [cash, setCash] = useState(0);
   const [inventory, setInventory] = useState<CivCard[]>([]);
+  const [manualDiscounts, setManualDiscounts] = useState<
+    Record<string, number>
+  >({});
 
-  const discounts = useMemo(
+  const cardDiscounts = useMemo(
     () =>
       inventory
         .flatMap((card) => [card.cardDiscount, ...card.colorDiscounts])
@@ -64,10 +99,24 @@ const App = () => {
     [inventory]
   );
 
+  const discounts: DiscountTuple[] = useMemo(
+    () => [
+      ...cardDiscounts,
+      ...Object.entries(manualDiscounts).map(([color, discount]) => ({
+        cardName: "COLOR",
+        color: color as Color,
+        discount,
+      })),
+    ],
+    [cardDiscounts, manualDiscounts]
+  );
+
   const cardsNotInInventory = useMemo(
     () => cards.filter((card) => !inventory.includes(card)),
     [inventory]
   );
+
+  console.debug({ manualDiscounts, discounts });
 
   const setsAvailable = useMemo(() => {
     return findSets(cardsNotInInventory, cash).sort(
@@ -88,6 +137,20 @@ const App = () => {
           min={0}
           max={1000}
         />
+      </div>
+      <h2>Discounts</h2>
+      <div className="manualDiscounts">
+        {DISCOUNT_NAMES.map((color) => (
+          <ManualDiscount
+            key={color}
+            color={color}
+            allDiscounts={discounts}
+            discount={manualDiscounts[color]}
+            setDiscount={(discount: number) =>
+              setManualDiscounts({ ...manualDiscounts, [color]: discount })
+            }
+          />
+        ))}
       </div>
       <h2>Inventory</h2>
       <h3>Add a card:</h3>
